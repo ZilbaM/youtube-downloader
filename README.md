fd5ddb956a9adb1a40fbcd2216faed0c9d1c3f49
Basile Maille <basmaille59@gmail.com>
2024-06-26 13:50:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Installation
+
+```bash
+pip install -r requirements.txt
+```
+
+## Running the server
+
+```bash
+flask run
+```
+
+Now you can access the [downloader](http://127.0.0.1:5000).
---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, render_template, flash, redirect, url_for
+from pytube import YouTube, Playlist
+import ffmpeg
+import os
+
+app = Flask(__name__)
+app.config['STATIC_FOLDER'] = 'static'
+app.secret_key = 'my_secret_key'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        youtube_url = request.form['youtube_url']
+        yt = YouTube(youtube_url)
+        stream = yt.streams.filter(only_audio=True).first()
+        stream.download(output_path='downloads')
+        audio_file = stream.default_filename
+        wav_file = audio_file.replace('.mp4', '.wav')
+        ffmpeg.input(f'downloads/{audio_file}').output(f'downloads/{wav_file}').run()
+        os.remove(f'downloads/{audio_file}')
+        flash('Audio downloaded and converted to WAV!')
+        return redirect(url_for('index'))
+    return render_template('./index.html')
+
+@app.route('/playlist', methods=['GET', 'POST'])
+def playlist():
+    if request.method == 'POST':
+        youtube_url = request.form['youtube_url']
+        playlist = Playlist(youtube_url)
+        for video in playlist.videos:
+            yt = YouTube(video.watch_url)
+            stream = yt.streams.filter(only_audio=True).first()
+            stream.download(output_path='downloads')
+            audio_file = stream.default_filename
+            wav_file = audio_file.replace('.mp4', '.wav')
+            ffmpeg.input(f'downloads/{audio_file}').output(f'downloads/{wav_file}').run()
+            os.remove(f'downloads/{audio_file}')
+        flash('All audio files in the playlist downloaded and converted to WAV!')
+        return redirect(url_for('index'))
+    return render_template('./playlist.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
